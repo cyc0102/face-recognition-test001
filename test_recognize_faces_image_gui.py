37fb3c3fcf19d52b3facdb93911b1163445a9078
Bryan <tw_bryan_chen@intretech.com>
2018-09-04 15:35:50 +0800

    add time record

---
+++ test_recognize_faces_image_gui.py
@@ -0,0 +1,107 @@
+# USAGE
+# python recognize_faces_image.py --encodings encodings.pickle --image examples/example_01.png 
+
+# import the necessary packages
+import face_recognition
+# import argparse
+import pickle
+import cv2
+from easygui import fileopenbox
+from datetime import datetime
+
+encoding_file_name = 'encodings.pickle'
+default_model ='cnn'
+
+'''
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+'''
+
+
+# load the known faces and embeddings
+print("[INFO] loading encodings...")
+
+data = pickle.loads(open(encoding_file_name, "rb").read())
+# print('data',data)
+
+# load the input image and convert it from BGR to RGB
+
+
+print("[INFO] Select input image ...")
+input_file=fileopenbox(msg='Select an image file...',title='Face Detection')
+image = cv2.imread(input_file)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# detect the (x, y)-coordinates of the bounding boxes corresponding
+# to each face in the input image, then compute the facial embeddings
+# for each face
+
+print("[INFO] recognizing faces...")
+
+t1= datetime.now()#测试起始时间
+boxes = face_recognition.face_locations(rgb, model=default_model)
+
+t2 = datetime.now()#测试结束时间
+print('time spend for face_detection (us): ',(t2-t1).microseconds)#显示总的时间开销
+
+# print('boxes=', boxes)	
+encodings = face_recognition.face_encodings(rgb, boxes)
+
+t3 = datetime.now()#测试结束时间
+print('time spend for face_encoding (us): ',(t3-t2).microseconds)#显示总的时间开销
+# print('encodings=', encodings)
+# initialize the list of names for each face detected
+names = []
+
+# loop over the facial embeddings
+for encoding in encodings:
+	# attempt to match each face in the input image to our known
+	# encodings
+	matches = face_recognition.compare_faces(data["encodings"],
+		encoding, tolerance=0.53)
+	name = "Unknown"
+
+	# check to see if we have found a match
+	if True in matches:
+		# find the indexes of all matched faces then initialize a
+		# dictionary to count the total number of times each face
+		# was matched
+		matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+		counts = {}
+
+		# loop over the matched indexes and maintain a count for
+		# each recognized face face
+		for i in matchedIdxs:
+			name = data["names"][i]
+			counts[name] = counts.get(name, 0) + 1
+
+		# determine the recognized face with the largest number of
+		# votes (note: in the event of an unlikely tie Python will
+		# select first entry in the dictionary)
+		name = max(counts, key=counts.get)
+	
+	# update the list of names
+	names.append(name)
+t4 = datetime.now()#测试结束时间
+print('time spend for face_recogniton (us): ',(t4-t3).microseconds)#显示总的时间开销
+# loop over the recognized faces
+for ((top, right, bottom, left), name) in zip(boxes, names):
+	# draw the predicted face name on the image
+	cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+	y = top - 15 if top - 15 > 15 else top + 15
+	cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+		0.75, (0, 255, 0), 2)
+
+# show the output image
+cv2.imshow("Image", image)
+t5 = datetime.now()#测试结束时间
+print('time spend for result_show (us): ',(t5-t4).microseconds)#显示总的时间开销
+print('time spend for total (us): ',(t5-t1).microseconds)#显示总的时间开销
+cv2.waitKey(0)
